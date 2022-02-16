dad09e344be5533f7683d4993fc7a9b8f3aebd28
Frank <frankvdv1998@gmail.com>
2022-02-16 19:05:36 +0100

    Initial commit

---
+++ src/practicumopdracht/models/DragQueen.java
@@ -0,0 +1,11 @@
+package practicumopdracht.models;
+
+public class DragQueen {
+    Show belongsTo;
+    String dragName;
+    String realName;
+    int age;
+    String gender;
+    String homeTown;
+    double salary;
+}
---
+++ src/practicumopdracht/models/Show.java
@@ -0,0 +1,10 @@
+package practicumopdracht.models;
+
+import java.time.LocalDate;
+
+public class Show {
+    String name;
+    String location;
+    LocalDate date;
+    boolean kidsFriendly;
+}
